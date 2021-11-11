bf3faa90bc91924c2c9cb6878398c1fc7bd1b0fd
krakerbrain <marioplantabaja@gmail.com>
2021-11-10 21:37:55 -0300

    Se crea función que va crea historial de chat en un archivo excel

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const qrcode = require("qrcode-terminal");
 const fs = require("fs");
 const { Client } = require("whatsapp-web.js");
 const chalk = require("chalk");
+const ExcelJS = require("exceljs");
+var moment = require("moment");
 
 const SESSION_FILE_PATH = "./session.json";
 let client;
@@ -49,19 +51,64 @@ const withOutSession = () => {
 const listenMessage = () => {
   client.on("message", (msg) => {
     try {
-      const { from, to, body } = msg;
-      console.log(from, to, body);
+      const { from, to, body, author } = msg;
+      saveHistorial(from, author);
     } catch (error) {
       console.log(error);
     }
   });
 };
+
 const sendMessage = (to, message) => {
   client.on("message", (message) => {
     if (message.body === "!ping") {
       message.reply("pong");
     }
   });
+};
+
+const saveHistorial = (number, message) => {
+  //location contiene el numero que identifica a los grupos de whatsapp
+  const location = number.slice(12, 22);
+  //pathChat crea el archivo excel que contendra la lista de usuarios
+  const pathChat = `./chats/ubicaciones.xlsx`;
+  const workbook = new ExcelJS.Workbook();
+  const today = moment().format("DD-MM-YYYY hh:mm");
+  if (location == 1626627314) {
+    if (fs.existsSync(pathChat)) {
+      workbook.xlsx.readFile(pathChat).then(() => {
+        const worksheet = workbook.getWorksheet(1);
+        const lastRow = worksheet.lastRow;
+        let getRowInsert = worksheet.getRow(++lastRow.number);
+        getRowInsert.getCell("A").value = today;
+        getRowInsert.getCell("B").value = message;
+        getRowInsert.commit();
+        workbook.xlsx
+          .writeFile(pathChat)
+          .then(() => {
+            console.log("Chat Agregado");
+          })
+          .catch(() => {
+            console.log("Algo Fallo");
+          });
+      });
+    } else {
+      const worksheet = workbook.addWorksheet("Chats");
+      worksheet.columns = [
+        { header: "Fecha", key: "date" },
+        { header: "Mensaje", key: "message" },
+      ];
+      worksheet.addRow([today, message]);
+      workbook.xlsx
+        .writeFile(pathChat)
+        .then(() => {
+          console.log("Historial creado");
+        })
+        .catch(() => {
+          console.log("Algo Fallo");
+        });
+    }
+  }
 };
 
 fs.existsSync(SESSION_FILE_PATH) ? withSession() : withOutSession();
